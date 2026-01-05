d521892b7273947586e3a4ed596a4ea997c78af0
naa <nigerianacademyofaudiology@gmail.com>
2026-01-05 12:25:39 +0100

    Update settings for ALLOWED_HOSTS

--- naa_site/settings.py
+++ naa_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 # Set DEBUG to False by default on Render
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*'] # We will tighten this later
+ALLOWED_HOSTS = ['naa-portal.onrender.com', 'localhost', '127.0.0.1'] # We will tighten this later
 
 
 # Application definition
