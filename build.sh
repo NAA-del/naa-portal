01f156e49ba1b4d40e1571331ec2fca2fc7f6850
naa <nigerianacademyofaudiology@gmail.com>
2026-01-05 11:42:05 +0100

    Force database migration on build

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# Force migrations to run
+python manage.py makemigrations accounts
+python manage.py migrate --no-input
+
+# Run the admin creation script
 python create_admin.py
