959500bf992984f1088a9cf2726e2afb7b9c6bd7
naa <nigerianacademyofaudiology@gmail.com>
2026-01-07 09:24:19 +0100

    Fixed static files and moved them to accounts app

--- naa_site/settings.py
+++ naa_site/settings.py
@@ -7,6 +7,12 @@ import os
 import dj_database_url
 from pathlib import Path
 
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder', # <--- This is the one that finds the accounts/static folder
+]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,15 +35,14 @@ INSTALLED_APPS = [
     # Cloudinary storage MUST be before django.contrib.staticfiles
     'cloudinary_storage',
     'cloudinary',
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    # Your App
+
     'accounts',
 ]
 
@@ -97,7 +102,11 @@ USE_TZ = True
 
 # --- STATIC FILES (CSS, JS, Medicio Assets) ---
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# settings.py
+STATICFILES_DIRS = [
+    BASE_DIR / "accounts" / "static",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # High-performance static serving for Render
