c80167aa72876afe21d8c1ebda62646f6b72a255
naa <nigerianacademyofaudiology@gmail.com>
2026-01-09 12:09:40 +0100

    Remove invalid create_admin script from build

--- build.sh
+++ build.sh
@@ -8,6 +8,3 @@ python manage.py collectstatic --no-input
 
 # This actually creates the tables in PostgreSQL
 python manage.py migrate --no-input
-
-# This creates your admin user
-python create_admin.pyg
