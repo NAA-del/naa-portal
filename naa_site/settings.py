7aa5d183b2a07e6848cb3739b8c2735a5a3bed86
naa <nigerianacademyofaudiology@gmail.com>
2026-01-10 17:02:44 +0100

    modified settings.py

--- naa_site/settings.py
+++ naa_site/settings.py
@@ -133,6 +133,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
